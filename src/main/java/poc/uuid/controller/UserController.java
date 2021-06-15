d132de574a22f56d573d6a0c406cedf3a9661fc9
Pedro Borges <pedromcborges11@gmail.com>
2021-06-15 11:40:57 -0300

    loading data via migration

--- src/main/java/poc/uuid/controller/UserController.java
+++ src/main/java/poc/uuid/controller/UserController.java
@@ -1,6 +1,7 @@
 package poc.uuid.controller;
 
 import com.github.javafaker.Faker;
+import javassist.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import poc.uuid.controller.response.UserResponse;
@@ -20,42 +21,49 @@ public class UserController {
 
     private final UserStringRepository userStringRepository;
     private final UserBinaryRepository userBinaryRepository;
-    private final Faker faker = new Faker();
 
     public UserController(UserStringRepository userStringRepository, UserBinaryRepository userBinaryRepository) {
         this.userStringRepository = userStringRepository;
         this.userBinaryRepository = userBinaryRepository;
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/string")
-    public UserString createString() {
-        final UserString userString = UserString.set(UUID.randomUUID().toString(), faker.name().name());
-        return userStringRepository.save(userString);
-    }
-
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/string")
-    public List<UserResponse> getUserStrings() {
+    public List<UserResponse> getStringUuids() {
         final var userString = userStringRepository.findAll();
         return userString.stream()
                 .map(it -> UserResponse.set(it.getId(), it.getName()))
                 .collect(Collectors.toList());
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/binary")
-    public UserBinary createBinary() {
-        final UserBinary userBinary = UserBinary.set(UuidHelper.getBytesFromUUID(UUID.randomUUID()), faker.name().name());
-        return userBinaryRepository.save(userBinary);
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/string/{id}")
+    public UserResponse getStringUuid(@PathVariable String id) throws NotFoundException {
+        final var userString = userStringRepository.findById(id);
+        if (userString.isPresent()) {
+            return UserResponse.set(userString.get().getId(), userString.get().getName());
+        } else {
+            throw new NotFoundException("Not found");
+        }
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/binary")
-    public List<UserResponse> getUserBinaries() {
+    public List<UserResponse> getBinaryUuids() {
         final var userBinary = userBinaryRepository.findAll();
         return userBinary.stream()
                 .map(it -> UserResponse.set(UuidHelper.getUUIDFromBytes(it.getId()).toString(), it.getName()))
                 .collect(Collectors.toList());
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/binary/{id}")
+    public UserResponse getBinaryUuid(@PathVariable String id) throws NotFoundException {
+        final var userBinary = userBinaryRepository.findById(UuidHelper.getBytesFromUUID(UUID.fromString(id)));
+        if (userBinary.isPresent()) {
+            return UserResponse.set(UuidHelper.getUUIDFromBytes(userBinary.get().getId()).toString(), userBinary.get().getName());
+        } else {
+            throw new NotFoundException("Not found");
+        }
     }
 }
--- src/main/resources/db/migration/V20210614142000__create_initial_tables.sql
+++ src/main/resources/db/migration/V20210614142000__create_initial_tables.sql
@@ -1,2 +1,52 @@
 create table user_string (id varchar(36) primary key not null, name varchar(255));
-create table user_binary (id binary(16) primary key not null, name varchar(255))
+create table user_binary (id binary(16) primary key not null, name varchar(255));
+
+DELIMITER #
+CREATE FUNCTION UUID_TO_BIN(uuid CHAR(36), f BOOLEAN)
+    RETURNS BINARY(16)
+    DETERMINISTIC
+BEGIN
+    RETURN UNHEX(CONCAT(
+            IF(f,SUBSTRING(uuid, 15, 4),SUBSTRING(uuid, 1, 8)),
+            SUBSTRING(uuid, 10, 4),
+            IF(f,SUBSTRING(uuid, 1, 8),SUBSTRING(uuid, 15, 4)),
+            SUBSTRING(uuid, 20, 4),
+            SUBSTRING(uuid, 25))
+        );
+END #
+DELIMITER ;
+
+delimiter #
+create procedure load_user_string_data()
+begin
+    declare v_max int unsigned default 1000000;
+    declare v_counter int unsigned default 0;
+
+    truncate table user_string;
+    start transaction;
+    while v_counter < v_max do
+            insert into user_string values (uuid(), 'name');
+            set v_counter=v_counter+1;
+        end while;
+    commit;
+end #
+delimiter ;
+
+delimiter #
+create procedure load_user_binary_data()
+begin
+    declare v_max int unsigned default 1000000;
+    declare v_counter int unsigned default 0;
+
+    truncate table user_binary;
+    start transaction;
+    while v_counter < v_max do
+            insert into user_binary values (UUID_TO_BIN(uuid(), true), 'name');
+            set v_counter=v_counter+1;
+        end while;
+    commit;
+end #
+delimiter ;
+
+call load_user_string_data();
+call load_user_binary_data();
